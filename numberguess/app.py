1ac19b83498970145d0b5240d5736b53eb6f1c1b
ReenyDavidson <52811027+ReenyDavidson@users.noreply.github.com>
2020-06-20 22:54:06 +0100

    Update app.py

--- numberguess/app.py
+++ numberguess/app.py
@@ -5,10 +5,12 @@ guesstaken = 0
 ran = random.randint(1,20)
 maxn =4
 
+#the condition remains valid as long as guesstaken is less than 5
 while guesstaken < 5:
     guess = int(input('what is the number(1,20): '))
 
     if guess > ran:
+        #increments on every attempt
         guesstaken +=1
         print('Number is too large, Try Again')
         continue
@@ -16,8 +18,9 @@ while guesstaken < 5:
     elif guesstaken == maxn:
         print(f'Game Over, The Correct Answer is {ran}')
         break
-
+ 
     elif guess < ran:
+        #increments on every attempt
         guesstaken +=1
         print('Number is too small, Try Again') 
         continue
